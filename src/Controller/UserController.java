bf9cfbd51cd3af19390f0967c40047d959dd4f99
Ray Jonathan <rayjonathan5415@gmail.com>
2022-11-22 06:34:26 +0700

    Update UserController.java

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -1,8 +1,9 @@
 package Controller;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import Models.*;
 import javax.swing.JOptionPane;
 
 public class UserController {
@@ -10,26 +11,26 @@ public class UserController {
     static ConnectDatabase conn = SingletonDatabase.getConnectObject();
 
     public void updateDB() {
+        Customer cust;
         conn.connect();
         String path1 = "UPDATE customer";
         String path2 = "UPDATE admin";
         String query = " SET firstname = ?, lastname = ?, password = ? WHERE username = ?";
-        if(){
+        if (cust.getStatus() == EnumStatusUser.CUSTOMER) {
             path1 += query;
             query = path1;
         } else {
             path2 += query;
             query = path2;
         }
-        try{
+        try {
             PreparedStatement stat = conn.con.prepareStatement(query);
-            
+
             JOptionPane.showMessageDialog(null, "Berhasil update barang ke database");
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error!! Gagal update barang ke database");
         }
         conn.disconnect();
     }
-    
+
 }
