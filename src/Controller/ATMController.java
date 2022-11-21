4c8a821e7e6297b3724d744be6cd463d429a2390
Ray Jonathan <rayjonathan5415@gmail.com>
2022-11-22 03:01:29 +0700

    ATM Done

--- src/Controller/ATMController.java
+++ src/Controller/ATMController.java
@@ -1,33 +1,47 @@
 package Controller;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
-import Models.Customer;
-
 public class ATMController {
 
-    static public void setBalance(double value, String username) {
-        Customer cust;
+    static public double showBalance(String username) {
+        double balance = 0;
+        ConnectDatabase conn = new ConnectDatabase();
+        conn.connect();
+        try {
+            java.sql.Statement stat = conn.con.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM customer WHERE username='" + username + "'");
+            if (result.next()) {
+                balance = result.getDouble("balance");
+            }
+            JOptionPane.showMessageDialog(null, "Berhasil menemukan data dari database");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error!! Gagal memasukkan data ke database");
+            System.out.println(e);
+        }
+        conn.disconnect();
+        return balance;
+    }
 
+    static public void setBalance(double value, String username) {
+        value += showBalance(username);
         ConnectDatabase conn = new ConnectDatabase();
         conn.connect();
         try {
             PreparedStatement stat = conn.con.prepareStatement(
-                    "UPDATE customer SET balance= ?  WHERE username ='" + username + "'");
+                    "UPDATE customer SET balance= ?  WHERE username = ? ");
             stat.setDouble(1, value);
+            stat.setString(2, username);
+            stat.executeUpdate();
             JOptionPane.showMessageDialog(null, "Berhasil memasukkan data ke database");
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error!! Gagal memasukkan data ke database");
             System.out.println(e);
         }
         conn.disconnect();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("WOI");
-        setBalance(0, "udin");
     }
 }
