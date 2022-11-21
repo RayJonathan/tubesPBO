2d7abac6bbf4e1cb1d8fcbf3c390eb70e5809c1a
kevinnp1093 <if-21009@students.ithb.ac.id>
2022-11-22 01:58:16 +0700

    Update CustomerController.java

--- src/Controller/CustomerController.java
+++ src/Controller/CustomerController.java
@@ -3,12 +3,14 @@ package Controller;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MemberController {
+import Models.Discount;
+
+public class CustomerController {
     public static String joinMember(String user) {
         ConnectDatabase conn = new ConnectDatabase();
         conn.connect();
         String output = "";
-        String query = "UPDATE customer SET isMember ='" + 1 + "'";
+        String query = "UPDATE customer SET isMember ='" + 1 + "'" + "WHERE id_cust ='" + user + "'";
         try {
             Statement stmt = conn.con.createStatement();
             stmt.executeUpdate(query);
@@ -18,5 +20,33 @@ public class MemberController {
         }
         conn.disconnect();
         return output;
+    }
+    public static int gachaDiscount(Discount[] discount){
+        ConnectDatabase conn = new ConnectDatabase();
+        conn.connect();
+        String output = "";
+        String query = "SELECT * FROM discount";
+
+    }
+    public static String reserveTable(String reserve, String table, String user){
+        ConnectDatabase conn = new ConnectDatabase();
+        conn.connect();
+        String output = "";
+        String query = "INSERT INTO reservation VALUES ('" + reserve + "'" + table + "'" + user + "')";
+        try {
+            Statement stmt = conn.con.createStatement();
+            stmt.executeUpdate(query);
+            output = "Berhasil memasukan data";
+        } catch (SQLException e) {
+            output = "Gagal memasukan data";
+        }
+        conn.disconnect();
+        return output;
+    }
+    public static String orderMenu(){
+        
+    }
+    public static String checkTransaction(){
+
     }
 }
