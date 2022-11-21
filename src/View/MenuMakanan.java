c9f0036847228b4251b1b5b4d89ce88db56b2343
Ray Jonathan <rayjonathan5415@gmail.com>
2022-11-22 04:01:43 +0700

    Update MenuMakanan.java

--- src/View/MenuMakanan.java
+++ src/View/MenuMakanan.java
@@ -12,17 +12,34 @@ public class MenuMakanan {
     static private ArrayList<String> menu = new ArrayList<>();
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
-        for (int i = 0; i < menu.size(); i++) {
+        showFood();
+        JFrame frame = new JFrame();
 
+        JPanel board = new JPanel();
+
+        board.setLayout(null);
+        board.setBounds(0, 0, 500, 500);
+
+        for (int i = 0, x = 0; i < menu.size(); i++, x += 50) {
+            for (int j = 0, y = 0; j < menu.size(); j++, y += 50) {
+                JLabel lab = new JLabel(" " + menu.get(j));
+                lab.setOpaque(true);
+                lab.setBounds(x, y, 50, 50);
+                board.add(lab);
+            }
         }
-        JLabel menu1 = new JLabel();
-        JButton b = new JButton("click");
-        b.setBounds(130, 100, 100, 40);
-        f.add(b);
-        f.setSize(400, 500);
-        f.setLayout(null);
-        f.setVisible(true);
+
+        frame.setSize(400, 400);
+        JLabel ladder = new JLabel();
+        ladder.setBounds(0, 0, 50, 200);
+        ladder.setOpaque(true);
+
+        JLayeredPane pane = frame.getLayeredPane();
+
+        pane.add(ladder, new Integer(2)); // front
+        pane.add(board, new Integer(1)); // back
+
+        frame.setVisible(true);
     }
 
     public static void showFood() {
