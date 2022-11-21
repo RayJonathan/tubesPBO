8173118b4de19a0bb2514236a9c870c0a7314f86
Ray Jonathan <rayjonathan5415@gmail.com>
2022-11-22 05:20:13 +0700

    Update MenuMakanan.java

--- src/View/MenuMakanan.java
+++ src/View/MenuMakanan.java
@@ -9,69 +9,96 @@ import javax.swing.*;
 import Controller.*;
 
 public class MenuMakanan {
-    <<<<<<<HEAD
     static ConnectDatabase conn = SingletonDatabase.getConnectObject();
 
     static private ArrayList<String> menu = new ArrayList<>();
 
-    =======
-    static private ArrayList<String> menu = new ArrayList<>();
+    // public static void main(String[] args) {
+    // showFood();
+    // JFrame frame = new JFrame("Menu Makanan");
+    // JPanel board = new JPanel();
+    // int counter = 0;
+    // board.setLayout(null);
+    // board.setBounds(0, 0, 1500, 990);
+    // int x = 20;
+    // frame.setSize(500, 500);
+    // JButton submit = new JButton("SUBMIT");
+    // submit.setBounds(350, counter, 130, 50);
+    // board.add(submit);
+    // JLabel ladder = new JLabel();
+    // ladder.setBounds(0, 0, 50, 200);
+    // ladder.setOpaque(true);
+    // JLayeredPane pane = frame.getLayeredPane();
+    // pane.add(ladder, new Integer(2)); // front
+    // pane.add(board, new Integer(1)); // back
+    // frame.setVisible(true);
+    // }
 
-    static ConnectDatabase conn = SingletonDatabase.getConnectObject();>>>>>>>00e0 ee123f8097f3f17a152a833ad013ebfecc08
-
-    public static void main(String[] args) {
+    public static void main(String[] Args) {
         showFood();
         JFrame frame = new JFrame("Menu Makanan");
 
-        JPanel board = new JPanel();
-        int counter = 0;
-        board.setLayout(null);
-        board.setBounds(0, 0, 1500, 990);
-        int x = 20;
-        for (int i = 0, y = 20; i < menu.size(); i++, y += 50) {
+        JLabel label0 = new JLabel(menu.get(0));
+        JLabel label1 = new JLabel(menu.get(1));
+        JLabel label2 = new JLabel(menu.get(2));
+        JLabel label3 = new JLabel(menu.get(3));
+        JLabel label4 = new JLabel(menu.get(4));
+        JLabel label5 = new JLabel(menu.get(5));
+        JLabel label6 = new JLabel(menu.get(6));
+        JLabel label7 = new JLabel(menu.get(7));
 
-            JLabel lab = new JLabel(" " + menu.get(i));
-            JCheckBox check = new JCheckBox();
-            JTextField quantity = new JTextField();
-            quantity.setBounds(x + 150 + 75, y, 75, 30);
-            check.setBounds(x + 150, y, 75, 30);
-            lab.setBounds(x, y, 145, 30);
-            board.add(check);
-            board.add(lab);
-            board.add(quantity);
-            System.out.println(y);
-            counter += y;
-        }
-        frame.setSize(500, 500);
-        JLabel ladder = new JLabel();
-        ladder.setBounds(0, 0, 50, 200);
-        ladder.setOpaque(true);
-        JLayeredPane pane = frame.getLayeredPane();
-        pane.add(ladder, new Integer(2)); // front
-        pane.add(board, new Integer(1)); // back
-        JButton submit = new JButton("SUBMIT");
-        submit.setBounds(250, counter, 130, 50);
-        board.add(submit);
+        JTextField qty0 = new JTextField();
+        JTextField qty1 = new JTextField();
+        JTextField qty2 = new JTextField();
+        JTextField qty3 = new JTextField();
+        JTextField qty4 = new JTextField();
+        JTextField qty5 = new JTextField();
+        JTextField qty6 = new JTextField();
+        JTextField qty7 = new JTextField();
+
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+        qty0.setBounds(170, 20, 150, 30);
+
+        label0.setBounds(20, 20, 150, 30);
+        label1.setBounds(20, 60, 150, 30);
+        label2.setBounds(20, 150, 150, 30);
+        label3.setBounds(20, 140, 150, 30);
+        label4.setBounds(20, 180, 150, 30);
+        label5.setBounds(20, 220, 150, 30);
+        label6.setBounds(20, 260, 150, 30);
+        label7.setBounds(20, 300, 150, 30);
+
+        frame.add(label0);
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(label4);
+        frame.add(label5);
+        frame.add(label6);
+        frame.add(label7);
+
+        frame.add(qty0);
+        frame.add(qty1);
+        frame.add(qty2);
+        frame.add(qty3);
+        frame.add(qty4);
+        frame.add(qty5);
+        frame.add(qty6);
+        frame.add(qty7);
+
+        frame.setSize(800, 800);
         frame.setVisible(true);
+
     }
-
-    <<<<<<<HEAD
-    // public static void main(String[] Args) {
-    // showFood();
-    // JFrame frame = new JFrame("Menu Makanan");
-    // JLabel label1 = new JLabel(menu.get(0));
-    // label1.setBounds(20, 20, 50, 30);
-    // frame.setSize(400, 400);
-    // frame.add(label1);
-    // frame.setVisible(true);
-
-    // }
 
     public static void showFood() {
         ConnectDatabase conn = new ConnectDatabase();
-
-    public static String showFood() {
-        ArrayList<String> menu = new ArrayList<>();
         conn.connect();
 
         try {
@@ -79,8 +106,7 @@ public class MenuMakanan {
             ResultSet rs = stat.executeQuery("SELECT * FROM menu");
             while (rs.next()) {
                 menu.add(rs.getString("name_menu"));
-       
-
+            }
             rs.toString();
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error!! Gagal memasukkan data ke database");
