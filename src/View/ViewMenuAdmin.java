adf13f4fd759e4f4d6c0db78f64c2f31a773a089
Bray <bryanimanuell17@gmail.com>
2022-11-22 07:42:41 +0700

    Update ViewMenuAdmin.java

--- src/View/ViewMenuAdmin.java
+++ src/View/ViewMenuAdmin.java
@@ -1,20 +1,11 @@
 package View;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
 import javax.swing.*;
 
 import Controller.AdminController;
-import Controller.ConnectDatabase;
-import Controller.SingletonDatabase;
-import Models.Menu;
-import Models.QueueTable;
 
 public class ViewMenuAdmin {
-    static ConnectDatabase conn = SingletonDatabase.getConnectObject();
+    JButton buttonTotalIncome, buttonTotalSale, buttonManageQueue, buttonAddMenu, buttonUpdateStatusFood;
     public ViewMenuAdmin() {
         JFrame f = new JFrame("Menu Admin");
         
@@ -42,6 +33,7 @@ public class ViewMenuAdmin {
         f.add(buttonUpdateStatusFood);
         f.add(buttonLogout);
 
+        f.setSize(400, 500);
         buttonTotalIncome.addActionListener(e ->{
             JOptionPane.showMessageDialog(null, 
             "Total seluruh pendapatan sebesar: " + AdminController.totalIncome());
@@ -52,13 +44,12 @@ public class ViewMenuAdmin {
             "Total penjualan: \n" + AdminController.totalSale());
         });
 
-        buttonManageQueue.addActionListener(e ->{});
-
-        buttonAddMenu.addActionListener(e ->{
-            f.dispose();
-            AddMenu();
+        buttonManageQueue.addActionListener(e ->{
+            
         });
 
+        buttonAddMenu.addActionListener(e ->{});
+        
         buttonUpdateStatusFood.addActionListener(e ->{});
 
         buttonLogout.addActionListener(e ->{
@@ -66,152 +57,6 @@ public class ViewMenuAdmin {
             new ViewMenuUtama();
         });
 
-        f.setSize(400, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    public void AddMenu() {
-        JFrame f = new JFrame("Add Menu");
-        
-        JLabel labTitle = new JLabel("Add New Menu");
-        JLabel labIdMenu = new JLabel("Id Menu:");
-        JLabel labNameMenu = new JLabel("Menu Name:");
-        JLabel labCategory = new JLabel("Category:");
-        JLabel labPrice = new JLabel("Price:");
-
-        JTextField inpIdMenu = new JTextField();
-        JTextField inpNameMenu = new JTextField();
-
-        JRadioButton food = new JRadioButton("Food");
-        JRadioButton drink = new JRadioButton("Drink");
-        ButtonGroup category = new ButtonGroup();
-        category.add(food);
-        category.add(drink);
-
-        JTextField inpPrice = new JTextField();
-
-        JButton buttonAdd = new JButton("Add");
-        JButton buttonCancel = new JButton("Cancel");
-
-        labTitle.setBounds(160, 20, 100, 40);
-        labIdMenu.setBounds(50, 70, 100, 40);
-        labNameMenu.setBounds(50, 120, 100, 40);
-        labCategory.setBounds(50, 170, 100, 40);
-        labPrice.setBounds(50, 220, 100, 40);
-
-        inpIdMenu.setBounds(130, 75, 100, 25);
-        inpNameMenu.setBounds(130, 125, 100, 25);
-        food.setBounds(130, 170, 65, 25);
-        drink.setBounds(190, 170, 65, 25);
-        inpPrice.setBounds(130, 220, 105, 25);
-
-        buttonAdd.setBounds(90, 270, 75, 50);
-        buttonCancel.setBounds(210, 270, 75, 50);
-
-        f.add(labTitle);
-        f.add(labIdMenu);
-        f.add(labNameMenu);
-        f.add(labCategory);
-        f.add(labPrice);
-
-        f.add(inpIdMenu);
-        f.add(inpNameMenu);
-        f.add(food);
-        f.add(drink);
-        f.add(inpPrice);
-
-        f.add(buttonAdd);
-        f.add(buttonCancel);
-
-        buttonAdd.addActionListener(e ->{
-            String idMenu = inpIdMenu.getText();
-            String namaMenu = inpNameMenu.getText();
-            String categoryTemp = "";
-            if(food.isSelected()) {
-                categoryTemp = "Food";
-            } else if(drink.isSelected()) {
-                categoryTemp = "Drink";
-            }
-            double price = Double.parseDouble(inpPrice.getText());
-            
-            Menu newMenu = new Menu(idMenu, namaMenu, categoryTemp, price);
-
-            AdminController.addMenu(newMenu);
-
-            f.dispose();
-            new ViewMenuAdmin();
-        });
-
-        buttonCancel.addActionListener(e ->{
-            f.dispose();
-            new ViewMenuAdmin();
-        });
-
-
-        f.setSize(400, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    public void ManageQueue() {
-        conn.connect();
-        String query = "SELECT * FROM queue";
-        ArrayList<QueueTable> queue = new ArrayList<>();
-        try {
-            Statement stmt = conn.con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                int idQueue = rs.getInt("id_queue");
-                String idCust = rs.getString("id_cust");
-                int cap = rs.getInt("capacity");
-
-                queue.add(new QueueTable(idQueue, idCust, cap));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        conn.disconnect();
-        
-        JFrame f = new JFrame("Add Menu");
-        
-        ArrayList<JCheckBox> indexQueue = new ArrayList<>();
-        int y = 20;
-        for (int i = 0; i < queue.size(); i++) {
-            indexQueue.add(new JCheckBox(
-                "Id Queue: " + queue.get(i).getIdQueue()
-                + ", Id Customer: " + queue.get(i).getIdCust()
-                + ", Capacity: " + queue.get(i).getJumlahOrang()));
-            indexQueue.get(i).setBounds(50, y, 150, 25);
-            f.add(indexQueue.get(i));
-            y += 50;
-        }
-
-        JButton buttonDelete = new JButton("Delete");
-        JButton buttonCancel = new JButton("Cancel");
-
-        buttonDelete.setBounds(90, y, 75, 25);
-        buttonCancel.setBounds(210, y, 75, 25);
-
-        f.add(buttonDelete);
-        f.add(buttonCancel);
-
-        buttonDelete.addActionListener(e ->{
-            for (int i = 0; i < queue.size(); i++) {
-                if (indexQueue.get(i).isSelected()) {
-                    AdminController.deleteUser(queue.get(i).getIdQueue());
-                }
-            }
-            f.dispose();
-            new ViewMenuAdmin();
-        });
-
-        buttonCancel.addActionListener(e ->{
-            f.dispose();
-            new ViewMenuAdmin();
-        });
-
-        f.setSize(400, 500);
         f.setLayout(null);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
