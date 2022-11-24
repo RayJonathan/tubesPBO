4c1f8c0070a6a50db18d96855aa8375721e3a99e
kevinnp1093 <if-21009@students.ithb.ac.id>
2022-11-25 02:48:19 +0700

    Transaction

--- src/Models/ReceiptDetails.java
+++ src/Models/ReceiptDetails.java
@@ -5,5 +5,62 @@ public class ReceiptDetails {
     String idMenu;
     int quantity;
     String statusFoodProgress;
+    double total;
 
+    public ReceiptDetails(String idReceiptDetails, String idReceipt, String idMenu, int quantity, String statusFoodProgress, double total){
+        this.idReceiptDetails = idReceiptDetails;
+        this.idReceipt = idReceipt;
+        this.idMenu = idMenu;
+        this.quantity = quantity;
+        this.statusFoodProgress = statusFoodProgress;
+        this.total = total;
+    }
+
+    public String getIdReceiptDetails(){
+        return idReceiptDetails;
+    }
+
+    public void setIdReceiptDetails(String idReceiptDetails){
+        this.idReceiptDetails = idReceiptDetails;      
+    }
+
+    public String getIdReceipt(){
+        return idReceiptDetails;
+    }
+
+    public void setIdReceipt(String idReceipt){
+        this.idReceipt = idReceipt;      
+    }
+
+    public String getIdMenu(){
+        return idMenu;
+    }
+
+    public void setIdMenu(String idMenu){
+        this.idMenu = idMenu;      
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;      
+    }
+
+    public String getStatusFoodProgress(){
+        return statusFoodProgress;
+    }
+
+    public void setStatusFoodProgress(String statusFoodProgress){
+        this.statusFoodProgress = statusFoodProgress;      
+    }
+
+    public double getTotal(){
+        return total;
+    }
+
+    public void setTotal(double total){
+        this.total = total;      
+    }
 }
--- src/Models/Transaction.java
+++ src/Models/Transaction.java
@@ -2,27 +2,39 @@ package Models;
 
 public class Transaction {
     String idTransaction;
+    String idReciept;
+    String idDiscount;
     double total;
 
-    public Transaction(String idTransaction, double total) {
+    public Transaction(String idTransaction, String idReciept, String idDiscount, double total) {
         this.idTransaction = idTransaction;
+        this.idReciept = idReciept;
+        this.idDiscount = idDiscount;
         this.total = total;
     }
 
     public String getIdTransaction() {
         return idTransaction;
     }
-
     public void setIdTransaction(String idTransaction) {
         this.idTransaction = idTransaction;
     }
-
+    public String getIdReciept() {
+        return idReciept;
+    }
+    public void setIdReciept(String idReciept) {
+        this.idReciept = idReciept;
+    }
+    public String getIdDiscount() {
+        return idDiscount;
+    }
+    public void setIdDiscount(String idDiscount) {
+        this.idDiscount = idDiscount;
+    }
     public double getTotal() {
         return total;
     }
-
     public void setTotal(double total) {
         this.total = total;
     }
-
 }
